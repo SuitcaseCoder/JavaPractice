5cfabee581e5f276a90511144f6eb7d68bb51c1b
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-22 15:50:38 -0500

    first commit

---
+++ src/JuneJava.java
@@ -0,0 +1,52 @@
+// pulling practice problems from here: https://www.w3resource.com/java-exercises/basic/index.php
+
+import java.util.Scanner;
+
+public class JuneJava {
+    public static void main(String[] args) {
+////        Write a Java program to print 'Hello' on screen and then print your name on a separate line
+//        System.out.println("Hello");
+//        System.out.println("Laura");
+    
+
+////    2. Write a Java program to print the sum of two numbers.
+//        int num1 = 18;
+//        int num2 = 20;
+//        System.out.println(num1 + num2);
+    
+////    3. Write a Java program to divide two numbers and print on the screen
+//        int firstNum = 20;
+//        int secondNum = 8;
+//        System.out.println(firstNum/secondNum);
+    
+////      4. Write a Java program to print the result of the following operations
+////            Test Data with expected results::
+////a. -5 + 8 * 6 // 43
+////b. (55+9) % 9 // 1
+////c. 20 + -3*5 / 8 // 19
+////d. 5 + 15 / 3 * 2 - 8 % 3 // 13
+//
+//        System.out.println(-5 + 8 * 6);
+//        System.out.println((55+9) % 9);
+//        System.out.println(20 + -3 *5 / 8);
+//        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    
+//        5. Write a Java program that takes two numbers as input and display the product of two numbers
+            Scanner sc = new Scanner(System.in);
+        System.out.println("return two numbers");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
